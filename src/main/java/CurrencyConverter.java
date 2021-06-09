ad1dae0a61b49169f9cb0bcab5a886b0a8ddbdd3
cprudhvi <cprudhvi9@gmail.com>
2021-06-09 03:06:50 +0200

    Building with wrong code and wrong testcase ...!!!

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -4,14 +4,23 @@ public class CurrencyConverter {
     }
     public int dollar_to_rs(int dollar){
         int rs1 = dollar * 70;
-        return rs1;
+        //return rs1;
+        return 0;
     }
     public int sek_to_rs(int sek){
         int rs2 = sek * 9;
-        return rs2;
+        //return rs2;
+        return 0;
     }
     public int euro_to_rs(int euro){
         int rs3 = euro * 100;
-        return rs3;
+        //return rs3;
+        return 0;
     }
 }
+
+//hello wecome to task driven development screen recording....
+// previously we have checked wheather the environment is working or not..
+//in this video we are going to perform task driven development in my continous integration project..!!!
+// frist we will test with wrong code and wrong test cases..!!
+//the above code is modived such that it does no work.
--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -12,17 +12,18 @@ public class TestCurrencyConverter {
 
     @Test
     public void test_dlr_to_rs(){
-        assertEquals(350, cc.dollar_to_rs(5));
+        assertEquals(35, cc.dollar_to_rs(5));
     }
 
     @Test
     public void test_sek_to_rs(){
-        assertEquals(90, cc.sek_to_rs(10));
+        assertEquals(9, cc.sek_to_rs(10));
     }
 
     @Test
     public void test_euro_to_rs(){
-        assertEquals(300, cc.euro_to_rs(3));
+        assertEquals(30, cc.euro_to_rs(3));
     }
 }
-// this is the screen recording to show that the environment works with the code and a few test cases that i have written to test in this project.
+
+//the above test case code is modified such that it doesnot give expectd results..! now lets build the code..
