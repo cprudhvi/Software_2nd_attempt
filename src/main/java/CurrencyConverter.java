40dd4477b9fc3587f7d87fb791b4185ee570e1eb
cprudhvi <cprudhvi9@gmail.com>
2021-06-09 03:11:12 +0200

    Trying to rebuild the code after modifying the code as a part to test driven development..!!!

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -4,17 +4,14 @@ public class CurrencyConverter {
     }
     public int dollar_to_rs(int dollar){
         int rs1 = dollar * 70;
-        //return rs1;
         return 0;
     }
     public int sek_to_rs(int sek){
         int rs2 = sek * 9;
-        //return rs2;
         return 0;
     }
     public int euro_to_rs(int euro){
         int rs3 = euro * 100;
-        //return rs3;
         return 0;
     }
 }
@@ -24,3 +21,5 @@ public class CurrencyConverter {
 //in this video we are going to perform task driven development in my continous integration project..!!!
 // frist we will test with wrong code and wrong test cases..!!
 //the above code is modived such that it does no work.
+//now we are making changes to the code and re buildin the project to see if it works with changing the code in the
+//project ...!!!!
